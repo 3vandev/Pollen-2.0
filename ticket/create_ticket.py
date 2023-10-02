dcbfbac5859ec804f3ecd10ef00e3b31f8d1d2dd
3van <119163823+3van-Reid@users.noreply.github.com>
2023-10-02 06:17:23 +0100

    Ticket Tool
    Pollen now has a built in ticket tool!

--- ticket/create_ticket.py
+++ ticket/create_ticket.py
@@ -2,15 +2,18 @@ import discord
 import os
 from .close_ticket import close_ticket
 from utilitys.embed_builder import embed_builder
-from importlib.machinery import SourceFileLoader
+from discord.interactions import Interaction
+from discord import ui
 
-class create_ticket(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
-        self.value = None
-     
-    @discord.ui.button(label="Create Ticket", style=discord.ButtonStyle.gray, custom_id="POLLEN-TICKET-CREATE",emoji="🎫")
-    async def ticket_create(self, interaction: discord.Interaction, button: discord.ui.Button):
+class create(ui.Modal, title="Open a ticket"):
+    reason = ui.TextInput(
+        label="Whats your reason for opening a ticket?",
+        style=discord.TextStyle.long,
+        placeholder="Reason",
+        required=True,
+    )
+
+    async def on_submit(self, interaction: Interaction):
         # create a new text channel with the name "ticket-{user's name}"
         channel = await interaction.guild.create_text_channel(f"ticket-{interaction.user.name}")
         
@@ -22,4 +25,14 @@ class create_ticket(discord.ui.View):
 
         # send an embed message to the channel with the user's name and a message for them to be patient
         # also include the close_ticket view to allow the user to close the ticket
-        await channel.send(embed=embed_builder(f"{interaction.user.name}'s Ticket", "Moderators will be with you shortly please be patient"), view=close_ticket())
+        await channel.send(embed=embed_builder(f"{interaction.user.name}'s Ticket", f"**Reason:**\n{self.reason}\n\nModerators will be with you shortly please be patient"), view=close_ticket())
+        await interaction.response.send_message(embed=embed_builder("Success", "Ticket created!"), ephemeral=True)
+
+class create_ticket(discord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+        self.value = None
+     
+    @discord.ui.button(label="Create Ticket", style=discord.ButtonStyle.gray, custom_id="POLLEN-TICKET-CREATE",emoji="🎫")
+    async def ticket_create(self, interaction: discord.Interaction, button: discord.ui.Button):
+        await interaction.response.send_modal(create())
--- ticket/setup_ticket.py
+++ ticket/setup_ticket.py
@@ -7,6 +7,42 @@ from .create_ticket import create_ticket
 from .delete_ticket import delete_ticket
 from .close_ticket import close_ticket
 from utilitys.embed_builder import embed_builder
+from discord.interactions import Interaction
+from discord import ui
+
+class create(ui.Modal, title="Create a ticket panel"):
+    header = ui.TextInput(
+        label="Title of the ticket panel",
+        style=discord.TextStyle.short,
+        placeholder="Title",
+        required=True,
+    )
+
+    description = ui.TextInput(
+        label="Description of the ticket panel",
+        style=discord.TextStyle.long,
+        placeholder="Description",
+        required=True,
+    )
+
+    async def on_submit(self, interaction: Interaction):
+        # create a new text channel with the name "ticket-{user's name}"
+        # Use the class create_ticket to create a view and button
+        view = create_ticket()
+
+        # Create the embed
+        embed = discord.Embed(
+            colour=discord.Colour.yellow(),
+            description=self.description,
+            title=self.header
+        )
+        embed.add_field(name="Ticket", value="Press the button bellow to create a ticket")
+
+        # Send the embed and view to the channel
+        await interaction.channel.send(embed=embed, view=view)
+
+        # Send a message to the user that the panel has been created
+        await interaction.response.send_message("Ticket Panel Created!", ephemeral=True)
 
 class setup_ticket(commands.Cog):
     def __init__(self, client: commands.bot):
@@ -21,21 +57,7 @@ class setup_ticket(commands.Cog):
             await interaction.response.send_message(embed=embed_builder("Ticket", "You need the administrator permission to use this command"), ephemeral=True)
             return
 
-        # Use the class create_ticket to create a view and button
-        view = create_ticket()
-
-        # Create the embed
-        embed = discord.Embed(
-            colour=discord.Colour.yellow()
-        )
-        embed.add_field(name="Ticket", value="Press the button bellow to create a ticket")
-        embed.set_footer(text="Pollen - Lightweight Discordbot",icon_url=self.client.user.avatar.url)
-
-        # Send the embed and view to the channel
-        await interaction.channel.send(embed=embed, view=view)
-
-        # Send a message to the user that the panel has been created
-        await interaction.response.send_message("Ticket Panel Created!", ephemeral=True)
+        await interaction.response.send_modal(create())
         
 # Defining a setup function to add the cog and views to the bot
 async def setup(client: commands.Bot) -> None:
